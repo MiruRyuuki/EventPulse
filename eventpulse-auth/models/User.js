be8c870f5a0869780a34fe72cac4d80e44cdac5f
MiruRyuuki <miruryuuki@gmail.com>
2025-07-22 13:07:59 +0800

    test

--- eventpulse-auth/models/User.js
+++ eventpulse-auth/models/User.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: String,
   password: { type: String, required: true },
-  role: { type: String, enum: ['Organizer', 'Speaker', 'Attendee', 'Reviewer', 'Admin'] }
+  role: { type: String, enum: ["Organizer", "Speaker", "Attendee", "Reviewer", "Admin"], required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
